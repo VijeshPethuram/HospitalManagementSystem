010fbd6f2c2fdc6b601be1b92ff47821bd70c450
VijeshPethuram <92882475+VijeshPethuram@users.noreply.github.com>
2023-07-16 21:24:03 +0530

    Update ConnectionProvider.java

--- src/javaminipro/ConnectionProvider.java
+++ src/javaminipro/ConnectionProvider.java
@@ -14,7 +14,7 @@ public class ConnectionProvider {
     try
     {
     Class.forName("com.mysql.cj.jdbc.Driver");
-    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/javaminipro","root","vijesh863748");
+    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/javaminipro","root","pass");
     return con;
     }
     catch(Exception e)
